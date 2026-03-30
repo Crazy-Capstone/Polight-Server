a25bd3f8e88296ed38e467ccca229d67f2dc087a
molcham <130418659+molcham@users.noreply.github.com>
2026-03-30 12:40:00 +0900

    chore:초기 환경 세팅

---
+++ src/main/java/polight/server/PolightServerApplication.java
@@ -0,0 +1,13 @@
+package polight.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolightServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PolightServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/domain/polightserver/PolightServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domain.polightserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolightServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
