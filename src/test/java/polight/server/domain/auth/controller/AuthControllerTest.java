7bb0c52c928e37797a85354ebffa2c4e0a8499af
molcham <130418659+molcham@users.noreply.github.com>
2026-06-02 16:24:20 +0900

    feat : 카카오 로그인 jwt 로직 구현 완료

--- src/test/java/polight/server/domain/auth/controller/AuthControllerTest.java
+++ src/test/java/polight/server/domain/auth/controller/AuthControllerTest.java
@@ -7,23 +7,28 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import polight.server.domain.auth.dto.AuthTokenResponse;
 import polight.server.domain.auth.service.AuthService;
+import polight.server.global.security.JwtTokenProvider;
 
 @WebMvcTest(AuthController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class AuthControllerTest {
 
   @Autowired private MockMvc mockMvc;
 
   @MockBean private AuthService authService;
 
+  @MockBean private JwtTokenProvider jwtTokenProvider;
+
   @Test
   void kakaoLogin_acceptsAuthorizationCodeOnly() throws Exception {
-    given(authService.loginWithKakao(anyString())).willReturn(new AuthTokenResponse("token", 3600));
+    given(authService.loginWithKakao(anyString())).willReturn(new AuthTokenResponse("token", 3600L));
 
     mockMvc
         .perform(
